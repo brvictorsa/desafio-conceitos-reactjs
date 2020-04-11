67788d4aa7a6cf4d39fed07fa4f603bddae2be31
Victor Santos <victorsa.mg@gmail.com>
2020-04-11 10:54:55 -0300

    Removida variável não utilizada

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    const response = await api.delete(`repositories/${id}`);
+    await api.delete(`repositories/${id}`);
     
     const repos = repositories.filter(repo => repo.id !== id);
     
